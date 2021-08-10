d405feec780ea1dbd3a072b0cdb839800c5dcd1d
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-08-10 14:15:20 +0000

    Styling the DayListItem

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import "components/DayListItem.scss";
+
+const classNames = require('classnames');
 
 export default function DayListItem(props) {
+
+  const DayListItemClass = classNames('day-list__item', {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots === 0
+  })
   return (
     // The <li> represents the entire day item
     // We are passing setDay, wrapped in anonymous func 
-    <li onClick={() => props.setDay(props.name)}>
+    <li className={DayListItemClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2> 
       <h3 className="text--light">{props.spots}</h3>
     </li>
