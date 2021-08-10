eb183e67139a0af79c52d69c77b4f089e36ec1a5
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-08-10 14:34:21 +0000

    Testing the DayListItem with Jest

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -9,12 +9,28 @@ export default function DayListItem(props) {
     "day-list__item--selected": props.selected,
     "day-list__item--full": props.spots === 0
   })
+
+  const formatSpots = function(spots) {
+    // if (spots === 0) {
+    //   return "no spots remaining";
+    // } else if (spots === 1) {
+    //   return `${spots} spot remaining`;
+    // } else {
+    //   return `${spots} spots remaining`;
+    // }
+    // OR:
+    return (
+      spots === 0 ? "no spots remaining" : `${spots} spot${spots === 1 ? '' : 's'} remaining`
+    )
+  }
+
   return (
     // The <li> represents the entire day item
     // We are passing setDay, wrapped in anonymous func 
     <li className={DayListItemClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2> 
-      <h3 className="text--light">{props.spots}</h3>
+      {/* <h3 className="text--light">{props.spots}</h3>  for test need to update this: */}
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
   )
 };
