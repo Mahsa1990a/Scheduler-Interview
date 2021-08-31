122428a254129cd432c71119321ed92fea931351
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-08-31 18:02:28 -0400

    updated

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -4,21 +4,24 @@ import "components/InterviewerListItem.scss";
 const classNames = require('classnames');
 
 export default function InterviewerListItem(props) {
+  
+  const { name, avatar, selected, setInterviewer } = props;
 
   const interviewClass = classNames({
     interviewers__item: true,
-    "interviewers__item--selected": props.selected
+    // "interviewers__item--selected": props.selected
+    "interviewers__item--selected": selected
   });
 
   return (
     // <li className={interviewClass} onClick={() => props.setInterviewer(props.id)}> Update:
-    <li className={interviewClass} onClick={props.setInterviewer}>
+    <li className={interviewClass} onClick={setInterviewer}>
       <img
         className="interviewers__item-image"
-        src={props.avatar}
-        alt={props.name}
+        src={avatar}
+        alt={name}
       />
-      {props.selected && props.name}
+      {selected && name}
     </li>
   )
 }
